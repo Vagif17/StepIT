280a4b224a572adef6ec233a95c25cedb5892703
Vagif Aliev <you@example.com>
2023-04-07 19:11:31 +0400

    C++

---
+++ Laboratory works/30.03.2023(2)/Tasks/Task 1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "String.h"
+using namespace std;
+int main() {
+    // Task 1
+    char* word1 = "Hello"; // Можно изменить что бы убедиться что программа работает.
+    char* word2 = "World";
+    return  comparisons(word1,word2);
+}
