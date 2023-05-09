e8090b7f5c80fa5f4c3e1b305d06635eaa28da2e
Vg_Al <107853016+Vagif17@users.noreply.github.com>
2023-05-09 16:42:02 +0400

    Delete Laboratory works directory

--- Laboratory works/30.03.2023(2)/Tasks/Task 4.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "String.h"
-using namespace std;
-int main (){
-    // Task 4
-    char* word = "hello world";
-    cout << upperCase(word);
-    return 0;
-}
--- Laboratory works/30.03.2023(2)/Tasks/Task 6.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "String.h"
-using namespace std;
-int main(){
-    // Task 6
-    char* word = "Hello";
-    cout << reverse(word);
-    return 0;
-}
