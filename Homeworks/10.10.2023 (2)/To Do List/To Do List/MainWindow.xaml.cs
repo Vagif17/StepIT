5ac088ebba8b5f9a6abce03d24827772d1fbf17a
Vagif Aliev <you@example.com>
2023-10-16 20:01:38 +0400

    WPF

---
+++ Homeworks/10.10.2023 (2)/To Do List/To Do List/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace To_Do_List
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<ToDo> list = new();
+
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (datetaskPicker.SelectedDate == null || titlenameBox.Text == null || describtionsBox.Text == null) { return; }
+
+            ToDo tmp = new();
+
+            tmp.Date = datetaskPicker.SelectedDate;
+            tmp.Title = titlenameBox.Text;
+            tmp.Describtion = describtionsBox.Text;
+
+            tasksListBox.Items.Add(tmp);
+
+        }
+    }
+
+   
+    class ToDo
+    {
+        public String Title { get; set; }
+        
+        public DateTime? Date { get; set; }
+        
+        public String Describtion { get; set; }
+
+
+        public ToDo() { }
+
+        public override string ToString()
+        {
+            return $"Title : {Title}\t Date : {Date}\t Describtions : {Describtion}";
+        }
+    }
+}
