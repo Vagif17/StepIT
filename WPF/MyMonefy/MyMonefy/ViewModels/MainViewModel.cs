4d7d30c5128ad1b73ff33386374b66ed566bcc38
Vagif Aliev <you@example.com>
2023-11-23 14:55:39 +0400

    WPF

--- WPF/MyMonefy/MyMonefy/ViewModels/MainViewModel.cs
+++ WPF/MyMonefy/MyMonefy/ViewModels/MainViewModel.cs
@@ -26,8 +26,7 @@ class MainViewModel : ViewModelBase
 
     public MainViewModel(Services.Interfaces.INavigationService navigationService, IMessenger messenger)
     {
-        CurrentView = App.Container.GetInstance<BudgetViewModel>(); // ЗАМЕНИТЬ ПОТОМ НА CHOICEVIEWMODEL
-
+        CurrentView = App.Container.GetInstance<ChoiceViewModel>(); 
         _navigationService = navigationService;
         _messenger = messenger;
 
