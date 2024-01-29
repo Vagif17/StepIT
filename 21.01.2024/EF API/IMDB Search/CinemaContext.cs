79718e95779277f46bddcc6fb4f77a8a6dc3e625
Vagif Aliev <you@example.com>
2024-01-29 15:18:22 +0400

    EF

---
+++ 21.01.2024/EF API/IMDB Search/CinemaContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDB_Search;
+
+internal class CinemaContext : DbContext
+{
+    DbSet<Movie> Movies { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var builder = new ConfigurationBuilder();
+        builder.AddJsonFile("appsettings.json");
+
+        var confiq = builder.Build();
+
+        optionsBuilder.UseSqlServer(confiq.GetConnectionString("Default"));
+    }
+}
---
+++ 21.01.2024/EF API/IMDB Search/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using IMDB_Search;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IMDB_Search
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void titlesearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            var searched = FindMovie.GetMovie(titlesearchTextBox.Text);
+            imageone.Source = new BitmapImage (new Uri (searched.Search[0].Poster));
+            imagetwo.Source = new BitmapImage (new Uri(searched.Search[1].Poster));
+            imagethree.Source = new BitmapImage(new Uri(searched.Search[2].Poster));
+
+            CinemaContext cinema = new CinemaContext();
+
+            List<Movie> movies = new List<Movie>
+            {
+                new Movie { Title = searched.Search[0].Title, Year = searched.Search[0].Year},
+                new Movie { Title = searched.Search[1].Title, Year = searched.Search[1].Year},
+                new Movie { Title = searched.Search[2].Title, Year = searched.Search[2].Year }
+            }; 
+
+            cinema.AddRange(movies);
+
+            cinema.SaveChanges();
+
+        }
+    }
+       
+
+}
