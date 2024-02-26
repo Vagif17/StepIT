07cd4cadb346b3bac1fe8ce544716be2890ffcdf
Vagif Aliev <you@example.com>
2024-02-26 20:29:33 +0400

    EF

---
+++ EF/YuniYol/YuniYol/Messages/AllStoragesMessage.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Identity.Client.Extensions.Msal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YuniYol.Models;
+using Storage = YuniYol.Models.Storage;
+
+namespace YuniYol.Messages;
+
+internal class AllStoragesMessage
+{
+
+    public List<Storage> storages { get; set; }
+
+}
---
+++ EF/YuniYol/YuniYol/Messages/EmailToResetMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YuniYol.Messages;
+
+internal class EmailToResetMessage
+{
+    public string Email { get; set; }
+}
