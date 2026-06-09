a74dc98f55d5a2c630b9be28bcbf9f6f3285fe15
Vagif Aliev <you@example.com>
2026-06-09 15:11:17 +0400

    Convert RN2 from submodule to regular directory

---
+++ RN2/components/ProfileCard.jsx
@@ -0,0 +1,22 @@
+﻿import {Button, Text, View} from "react-native";
+import {useState} from "react";
+
+export default  function ProfileCard ({ Name , Description,}){
+    const [isSelected, setIsSelected] = useState(false);
+
+    return <View style={{
+        backgroundColor: isSelected ? "cyan" : "white",
+        padding: 16,
+        borderRadius: 8,
+        height: 90
+    }
+    }>
+        <Text>{Name}</Text>
+        <Text>{Description}</Text>
+
+
+        <Button
+            title={isSelected ? "Selected" : "Select"}
+            onPress={() => setIsSelected(!isSelected)}
+        />    </View>
+}
