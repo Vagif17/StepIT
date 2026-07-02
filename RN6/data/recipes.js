b7bfc4f9ab28ad0be5a8a6d71bdbdc7e853280fb
Vagif Aliev <you@example.com>
2026-07-02 15:02:05 +0400

    RN6

---
+++ RN6/data/recipes.js
@@ -0,0 +1,46 @@
+export const RECIPES = [
+    {
+        id: 1,
+        name: "Spaghetti Carbonara",
+        category: "Pasta",
+        area: "Italian",
+        thumb: "https://picsum.photos/id/292/500/300",
+        videoUrl: "https://youtu.be/3AAdKl1UYZs?si=GPkFqzzxYVgDQYVl",
+    },
+    {
+        id: 2,
+        name: "Chicken Curry",
+        category: "Chicken",
+        area: "Indian",
+        thumb: "https://picsum.photos/id/431/500/300",
+        videoUrl: "https://youtu.be/VVXe_OlKbGc?si=NSJKaE3Ofad-jSeN",
+
+    },
+    {
+        id: 3,
+        name: "Sushi",
+        category: "Seafood",
+        area: "Japanese",
+        thumb: "https://picsum.photos/id/488/500/300",
+        videoUrl: "https://youtu.be/ZzLPUoetSHw?si=TtfKwj_R1hUKDL1_",
+
+    },
+    {
+        id: 4,
+        name: "Tacos",
+        category: "Beef",
+        area: "Mexican",
+        thumb: "https://picsum.photos/id/1080/500/300",
+        videoUrl: "https://youtu.be/vmuLzYet9K4?si=SeD-3_10LnhLLz1q",
+
+    },
+    {
+        id: 5,
+        name: "Croissant",
+        category: "Dessert",
+        area: "French",
+        thumb: "https://picsum.photos/id/312/500/300",
+        videoUrl: "https://youtu.be/djnNkLi_K6E?si=Uqpffuf8jBM4zT5k",
+
+    }
+];
