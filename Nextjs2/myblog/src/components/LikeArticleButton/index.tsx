7f618d47ef13e906fe3bab4ee3536a1eb6c7f0ad
Vagif Aliev <you@example.com>
2026-07-14 18:32:35 +0400

    HW 2

---
+++ Nextjs2/myblog/src/components/LikeArticleButton/index.tsx
@@ -0,0 +1,37 @@
+'use client'
+import {useState} from "react";
+
+const LikeArticleButton = () => {
+  const [liked, setLiked] = useState(false);
+
+  const handleLike = () => {
+    setLiked(!liked);
+  };
+
+  return (
+    <button onClick={handleLike}
+      className="
+        mt-5
+        rounded-lg
+        border
+        border-zinc-700
+        bg-zinc-800
+        px-4
+        py-2
+        text-sm
+        font-medium
+        text-zinc-200
+        transition-all
+        duration-200
+        hover:border-lime-500
+        hover:bg-zinc-700
+        hover:text-lime-400
+        active:scale-95
+      "
+    >
+      {liked ? "❤️ Liked" : "🤍 Like"}
+    </button>
+  );
+};
+
+export default LikeArticleButton;
